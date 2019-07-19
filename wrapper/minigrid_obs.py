a81b63db286df51ae13f4ce1c89841dfa6ae67bc
dallonsi <damien.allonsius@univ-amu.fr>
2019-07-19 18:33:03 +0200

    observation for minigrid is now ready ! The wrappers need to be cleaned

--- wrapper/minigrid_obs.py
+++ wrapper/minigrid_obs.py
@@ -18,8 +18,43 @@ class ObservationZoneWrapper(gym.ObservationWrapper):
         self.observation_agent = None
         self.observation_option = None
 
-    def render(self, size=(512, 512), agent_render=True, close=False, blurred_render=False, gray_scale_render=False):
-        self.env.display_image(rgb_array=self.observation_agent)
+    def observation(self, observation):
+        vanilla = self.env.render(mode='rgb_array', highlight=False)  # to get the rgb image
+        img_option = vanilla.copy()
+        img_agent = vanilla.copy()
+
+        # option observation
+        img_option = ObservationZoneWrapper.gray_scale(img_option)
+        self.images_stack.append(img_option)
+        img_option_stacked = np.zeros((img_option.shape[0], img_option.shape[1],
+                                img_option.shape[2]*self.parameters["stack_images_length"]), dtype=np.float32)
+        index_image = 0
+        for i in range(0, self.images_stack.__len__()):
+            img_option_stacked[..., index_image:index_image+img_option.shape[2]] = self.images_stack[i]
+            index_image = index_image+img_option.shape[2]
+
+        # agent observation
+        img_agent = ObservationZoneWrapper.make_downsampled_image(img_agent, self.zone_size_agent_x,
+                                                                  self.zone_size_agent_y)
+        img_agent = ObservationZoneWrapper.sample_colors(img_agent, self.thresh_binary_agent)
+
+        # store the observations
+        self.observation_agent = img_agent
+        self.observation_option = img_option
+
+        return {'vanilla': vanilla, "agent": img_agent, "option": img_option_stacked}
+
+    @staticmethod
+    def sample_colors(image, threshold):
+        img = cv2.medianBlur(image, 1)
+        #_, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+        return img
+
+    @staticmethod
+    def gray_scale(image):
+        im = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) / 255
+        im = im.reshape((*im.shape, 1))  # reshape for the neural network for a2c
+        return im
 
     @staticmethod
     def make_downsampled_image(image, zone_size_x, zone_size_y):
@@ -34,40 +69,3 @@ class ObservationZoneWrapper(gym.ObservationWrapper):
         else:
             raise Exception("The gridworld " + str(len_x) + "x" + str(len_y) +
                             " can not be fragmented into zones " + str(zone_size_x) + "x" + str(zone_size_y))
-
-    def observation(self, observation):
-        img_option = self.env.render(mode='rgb_array', highlight=False)  # to get the rgb image
-        img_agent = img_option.copy()
-        img_option = ObservationZoneWrapper.gray_scale(img_option)
-        img_option = img_option/255
-
-        self.images_stack.append(img_option)
-
-        img_option_stacked = np.zeros((self.images_stack[0].shape[0], self.images_stack[0].shape[1],
-                                       self.images_stack[0].shape[2] * 4), dtype=np.float32)
-
-        for i in range(0, self.images_stack.__len__()):
-            img_option_stacked[..., i - 1:i] = self.images_stack[i]
-
-        # agent observation
-        img_agent = ObservationZoneWrapper.make_downsampled_image(img_agent, self.zone_size_agent_x,
-                                                                  self.zone_size_agent_y)
-        img_agent = ObservationZoneWrapper.sample_colors(img_agent, self.thresh_binary_agent)
-
-        # store the observations
-        self.observation_agent = img_agent
-        self.observation_option = img_option
-
-        return {"agent": img_agent, "option": img_option_stacked}
-
-    @staticmethod
-    def sample_colors(image, threshold):
-        img = cv2.medianBlur(image, 1)
-        _, img = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
-        return img
-
-    @staticmethod
-    def gray_scale(image):
-        im = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        im = im.reshape((*im.shape, 1))  # reshape for the neural network for a2c
-        return im
