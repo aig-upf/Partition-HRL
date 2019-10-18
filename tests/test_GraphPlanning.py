5f7f653873e3e4ea7f133a27957b7a63d44d02f8
dallonsi <damien.allonsius@univ-amu.fr>
2019-10-18 19:54:39 +0200

    added a new test for one main function of GraphPlanning

--- tests/test_GraphPlanning.py
+++ tests/test_GraphPlanning.py
@@ -12,7 +12,13 @@ class ManagerTest(unittest.TestCase):
                            "edge_cost": 0}
         self.graphs = []
         self.best_paths = []
+
+        # graph star
         self.set_graph_star()
+
+        # graph loop
+        self.graphs.append(self.get_graph_loop([1, 0.1, -1, -1, 0]))
+        self.best_paths.append([0, 1, 2])
 
     def set_graph_star(self):
         graph = GraphPlanningPolicyManager(parameters=self.parameters)
@@ -25,8 +31,29 @@ class ManagerTest(unittest.TestCase):
         self.graphs.append(graph)
         self.best_paths.append([0, 3, 1, 4, 2])
 
+    def get_graph_loop(self, values):
+        graph = GraphPlanningPolicyManager(parameters=self.parameters)
+        lv = len(values)
+        graph.states = range(lv)
+        graph.number_explorations = [0] * lv
+        graph.transitions = [[(i+1, values[i])] for i in range(lv - 1)] + [[(0, values[-1])]]
+        graph.current_state_index = 0
+        graph.current_path = []
+
+        return graph
+
     # ------------- The tests are defined here --------------
     def test_find_best_path(self):
         for graph, best_path in zip(self.graphs, self.best_paths):
             graph.set_best_path()
             self.assertListEqual(graph.current_path, best_path)
+
+    def test_positive_loop(self):
+        graph = self.get_graph_loop(values=[1] * 5)
+        try:
+            graph.set_best_path()
+            success = False
+        except AssertionError:
+            success = True
+
+        self.assertTrue(success)
